b7f83bc1191b4550bee8d66e4ac81cc2789e354c
Mehdi Payervand <mehdipayervand@outlook.com>
2024-11-11 08:35:45 +0330

    direct message

---
+++ RequestDirectMessage/DirectMessage.cs
@@ -0,0 +1,34 @@
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RequestDirectMessage;
+
+public class DirectMessage
+{
+    private const string UserName = "guest";
+    private const string Password = "guest";
+    private const string HostName = "localhost";
+
+    public async Task SendMessage()
+    {
+
+        var connectionFactory = new RabbitMQ.Client.ConnectionFactory()
+        {
+            UserName = UserName,
+            Password = Password,
+            HostName = HostName
+        };
+
+        var connection = await connectionFactory.CreateConnectionAsync();
+        IChannel channel = await connection.CreateChannelAsync();
+       
+        byte[] messageBuffer1 = Encoding.Default.GetBytes("Direct Message 1");
+        await channel.BasicPublishAsync("request.exchange","directexchange_key",messageBuffer1);
+        Console.WriteLine("Message 1 Sent");
+        
+        byte[] messageBuffer2 = Encoding.Default.GetBytes("Direct Message 2");
+        await channel.BasicPublishAsync("request.exchange","directexchange_key",messageBuffer2);
+        Console.WriteLine("Message 2 Sent");
+        
+    }
+}
---
+++ RequestDirectMessage/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace RequestDirectMessage;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var directMessage = new DirectMessage();
+
+       await directMessage.SendMessage();
+
+       Console.WriteLine("press any key to end!");
+       Console.ReadKey();
+    }
+}
