a7ae836a3f098ab5e59cddb9b0087bbe079a90b8
Mehdi Payervand <mehdipayervand@outlook.com>
2024-11-12 16:05:54 +0330

    direct message

---
+++ RequestDirectConsumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+
+namespace RequestDirectConsumer;
+
+class Program
+{
+    private const string UserName = "guest";
+    private const string Password = "guest";
+    private const string HostName = "localhost";
+
+    static async Task Main(string[] args)
+    {
+        ConnectionFactory connectionFactory = new
+            ConnectionFactory
+            {
+                HostName = HostName,
+                UserName = UserName,
+                Password = Password,
+            };
+        var connection = await connectionFactory.CreateConnectionAsync();
+        IChannel channel = await connection.CreateChannelAsync();
+
+        // accept only one unack-ed message at a time
+        // uint prefetchSize, ushort prefetchCount, bool global
+        await channel.BasicQosAsync(0, 1, false);
+        MessageReceiver messageReceiver = new MessageReceiver(channel);
+        await channel.BasicConsumeAsync("request.queue", false, messageReceiver);
+        
+        Console.ReadLine();
+    }
+}
